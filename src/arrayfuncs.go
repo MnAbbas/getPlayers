2cb81468d7ec682b707892a7fa24713b370be09b
Mohammad Naser Abbasabadi <mn_abbasabadi@yahoo.com>
2018-11-17 21:17:27 +0330

    MnAbbas

--- src/arrayfuncs.go
+++ src/arrayfuncs.go
@@ -13,30 +13,34 @@ func (p PlayerSlice) Less(i, j int) bool { return p[i].Name < p[j].Name }
 func (p PlayerSlice) Len() int           { return len(p) }
 func (p PlayerSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
-func (s PlayerSlice) Sorted() PlayerSlice {
-	sort.Sort(s)
-	return s
+//Sorted is for sorting using heap
+func (p PlayerSlice) Sorted() PlayerSlice {
+	sort.Sort(p)
+	return p
 }
 
-func (s PlayerSlice) SortAndConcat() PlayerSlice {
-	newarr := s.RemoveDuplicates()
+//SortAndConcat is for Sorting and Removing Duplicate and concat them
+func (p PlayerSlice) SortAndConcat() PlayerSlice {
+	newarr := p.RemoveDuplicates()
 	sort.Sort(newarr)
 	return newarr
 }
 
-func (players PlayerSlice) FormatOutPut() {
+//FormatOutPut is for formating output
+func (p PlayerSlice) FormatOutPut() {
 
-	for index := 0; index < len(players); index++ {
-		elemnt := players[index]
+	for index := 0; index < len(p); index++ {
+		elemnt := p[index]
 		fmt.Printf("%d. %v %v; %v; %v\n", index+1, elemnt.FirstName, elemnt.LastName, elemnt.Age, strings.Join(elemnt.ListTeam[:], ","))
 
 	}
 }
 
-func (players PlayerSlice) RemoveDuplicates() PlayerSlice {
+//RemoveDuplicates is for remove Duplicate itemss and concat them
+func (p PlayerSlice) RemoveDuplicates() PlayerSlice {
 	refhappend := map[string]Player{}
 	result := []Player{}
-	for _, v := range players {
+	for _, v := range p {
 		if exist := refhappend[v.ID]; exist.ID != "" {
 			teamname := []string{exist.ListTeam[0], v.ListTeam[0]}
 			v.ListTeam = teamname
